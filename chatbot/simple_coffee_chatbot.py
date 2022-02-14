4a4f6da40c3b512a8935c55b6199baa362885db3
kcall174 <kmacallaghan@gmail.com>
2022-02-14 16:19:57 -0500

    Update simple_coffee_chatbot.py

--- chatbot/simple_coffee_chatbot.py
+++ chatbot/simple_coffee_chatbot.py
@@ -1,13 +1,13 @@
 #!python3
 #this is an example of a simple chatbot
 
-# Defining our functions
+# Defining coffee_bot function:
 def coffee_bot():
   print('Welcome to The Coffee Shop')
   size = get_size()
   print(size)
 
-# Defining out get_size function
+# Defining get_size function:
 def get_size():
   res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
   if res == 'a':
@@ -19,5 +19,5 @@ def get_size():
   else:
     return get_size()
 
-# Calling our coffee_bot 
+# Calling our coffee_bot: 
 coffee_bot()
